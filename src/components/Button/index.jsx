ec66119d4cc09ef6bc2d695206cb2234fb5a4241
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-11-15 21:49:51 -0500

    feat: full version v2 xd

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,18 @@
+import PropTypes from 'prop-types'
 import './Button.css'
 
-const Button = ({ text, onClick, type }) => {
+const Button = ({ children, type, onClick }) => {
   return (
     <button className="Button" type={type} onClick={onClick}>
-      {text}
+      {children}
     </button>
   )
+}
+
+Button.propTypes = {
+  children: PropTypes.any,
+  type: PropTypes.string,
+  onClick: PropTypes.func
 }
 
 export { Button }
--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,7 +1,12 @@
+import PropTypes from 'prop-types'
 import './Heading.css'
 
 const Heading = ({ children }) => {
   return <h1 className="Heading">{children}</h1>
+}
+
+Heading.propTypes = {
+  children: PropTypes.any
 }
 
 export { Heading }
